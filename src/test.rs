54b2b7e6cb55c88f725e6d7d331cd5db85cfee47
Devin Jean <emailcruzjean@yahoo.com>
2024-06-19 11:59:32 -0500

    extra if chain test

--- src/test.rs
+++ src/test.rs
@@ -581,6 +581,72 @@ fn test_nested_if_4() {
 }
 
 #[test]
+fn test_nested_if_5() {
+    let proj = compile(include_str!("projects/nested-if-5.xml"), None).unwrap();
+    assert_eq!(proj, Project {
+        name: "untitled".into(),
+        role: "myRole".into(),
+        state_machines: [
+            ("something".into(), StateMachine {
+                states: [
+                    ("thing 1".into(), State {
+                        actions: [].into_iter().collect(),
+                        transitions: [
+                            Transition {
+                                condition: Some("t > 10 & t > 9 & t > 7".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: "thing 4".into(),
+                            },
+                            Transition {
+                                condition: Some("t > 10 & t > 9 & ~(t > 7)".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: "thing 3".into(),
+                            },
+                            Transition {
+                                condition: Some("t > 10 & ~(t > 9) & t > 8".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: "thing 2".into(),
+                            },
+                            Transition {
+                                condition: Some("t > 10 & ~(t > 9) & ~(t > 8)".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: "thing 5".into(),
+                            },
+                            Transition {
+                                condition: Some("~(t > 10)".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: "thing 0".into(),
+                            },
+                        ].into_iter().collect(),
+                    }),
+                    ("thing 0".into(), State {
+                        actions: [].into_iter().collect(),
+                        transitions: [].into_iter().collect(),
+                    }),
+                    ("thing 2".into(), State {
+                        actions: [].into_iter().collect(),
+                        transitions: [].into_iter().collect(),
+                    }),
+                    ("thing 3".into(), State {
+                        actions: [].into_iter().collect(),
+                        transitions: [].into_iter().collect(),
+                    }),
+                    ("thing 4".into(), State {
+                        actions: [].into_iter().collect(),
+                        transitions: [].into_iter().collect(),
+                    }),
+                    ("thing 5".into(), State {
+                        actions: [].into_iter().collect(),
+                        transitions: [].into_iter().collect(),
+                    }),
+                ].into_iter().collect(),
+                initial_state: None,
+            }),
+        ].into_iter().collect(),
+    });
+}
+
+#[test]
 fn test_if_fall_through_1() {
     let err = compile(include_str!("projects/if-fall-through-1.xml"), None).unwrap_err();
     assert_eq!(err, CompileError::NonTerminalTransition { state_machine: "something".into(), state: "thing 1".into() });
