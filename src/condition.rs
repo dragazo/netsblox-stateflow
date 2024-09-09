37130a98e6af214859723ce897b5c9dad8698475
Devin Jean <emailcruzjean@yahoo.com>
2024-09-09 13:16:28 -0500

    extra debug assert

--- src/condition.rs
+++ src/condition.rs
@@ -319,7 +319,7 @@ impl Condition {
         &self.0
     }
     pub fn atom(v: CompactString) -> Self {
-        assert!(v != "true" && v != "false");
+        debug_assert!(v != "true" && v != "false" && v != "");
         Condition(RawCondition::Atom(v))
     }
     pub fn constant(v: bool) -> Self {
