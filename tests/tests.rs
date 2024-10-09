5bc649aa1731595395197a6a9d09c92257e7bd48
Devin Jean <emailcruzjean@yahoo.com>
2024-10-08 19:37:23 -0500

    extra chain wait test

--- tests/tests.rs
+++ tests/tests.rs
@@ -5429,3 +5429,84 @@ fn test_wait_3() {
     });
     assert_complete(&proj);
 }
+
+#[test]
+fn test_wait_4() {
+    let proj = Project::compile(include_str!("projects/wait-4.xml"), None, Settings::default()).unwrap();
+    assert_eq!(proj, Project {
+        name: "wait".into(),
+        role: "myRole".into(),
+        state_machines: [
+            ("my state".into(), StateMachine {
+                variables: [].into_iter().collect(),
+                states: [
+                    ("start".into(), State {
+                        parent: None,
+                        transitions: [
+                            Transition {
+                                unordered_condition: Condition::atom("after(1, sec)".into()),
+                                ordered_condition: Condition::atom("after(1, sec)".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: Some("::junction-1::".into()),
+                            },
+                            Transition {
+                                unordered_condition: !Condition::atom("after(1, sec)".into()),
+                                ordered_condition: Condition::constant(true),
+                                actions: [].into_iter().collect(),
+                                new_state: None,
+                            },
+                        ].into_iter().collect(),
+                    }),
+                    ("::junction-1::".into(), State {
+                        parent: Some("start".into()),
+                        transitions: [
+                            Transition {
+                                unordered_condition: Condition::atom("after(2, sec)".into()),
+                                ordered_condition: Condition::atom("after(2, sec)".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: Some("::junction-0::".into()),
+                            },
+                            Transition {
+                                unordered_condition: !Condition::atom("after(2, sec)".into()),
+                                ordered_condition: Condition::constant(true),
+                                actions: [].into_iter().collect(),
+                                new_state: None,
+                            },
+                        ].into_iter().collect(),
+                    }),
+                    ("::junction-0::".into(), State {
+                        parent: Some("start".into()),
+                        transitions: [
+                            Transition {
+                                unordered_condition: Condition::atom("after(3, sec)".into()),
+                                ordered_condition: Condition::atom("after(3, sec)".into()),
+                                actions: [].into_iter().collect(),
+                                new_state: Some("stop".into()),
+                            },
+                            Transition {
+                                unordered_condition: !Condition::atom("after(3, sec)".into()),
+                                ordered_condition: Condition::constant(true),
+                                actions: [].into_iter().collect(),
+                                new_state: None,
+                            },
+                        ].into_iter().collect(),
+                    }),
+                    ("stop".into(), State {
+                        parent: None,
+                        transitions: [
+                            Transition {
+                                unordered_condition: Condition::constant(true),
+                                ordered_condition: Condition::constant(true),
+                                actions: [].into_iter().collect(),
+                                new_state: None,
+                            },
+                        ].into_iter().collect(),
+                    }),
+                ].into_iter().collect(),
+                initial_state: Some("start".into()),
+                current_state: None,
+            }),
+        ].into_iter().collect(),
+    });
+    assert_complete(&proj);
+}
